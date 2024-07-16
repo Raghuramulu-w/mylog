bf480db0bb44862ad56654b839a8c30a9557eec7
raghuramulu-w <raghu0711@gmail.com>
2024-07-16 18:31:58 +0530

    hai

---
+++ deletelogfiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SOURCE_DIR=/tmp/sourcefile.log
+#checking dir eit or not
+if [ ! -d SOURCE_DIR ]
+then
+    echo "file dos not exit "
+if
+#finding .log file more than 14 days
+FILESTODELETE=$(find $SOURCE_DIR  -type f -mtime+14 -name "*.log")
+while IFS= read -r line
+do 
+  echo "deleting file $line"
+ done <<< $FILESTODELETE 
