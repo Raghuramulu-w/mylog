b2477e16e112607ea0e77556524adc11be95a465
raghuramulu-w <raghu0711@gmail.com>
2024-07-18 10:57:37 +0530

    hai

--- disk.sh
+++ disk.sh
@@ -3,8 +3,8 @@ DISK_USAGE=$(df - hT | grep -vE 'tmp|File')
 DISK_TREADSHOLD=1
 while IFS= read line
 do 
- usage=$(echo $line |  awk '{print $6F}' | cut -d % -f1)
- partition=$(echo $line | awk '{print $1F}')
+ usage=$(echo $line |  awk '{print$6F}' | cut -d % -f1)
+ partition=$(echo $line | awk '{print$1F}')
  if [ $partition -ge $DISK_TREADSHOLD ]
  then
  echo "high usage $partition : $usage"
