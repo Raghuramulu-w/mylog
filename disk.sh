684396626e1469fc77cf400f89aaf8bd29e5baa6
raghuramulu-w <raghu0711@gmail.com>
2024-07-18 09:59:00 +0530

    hai

--- disk.sh
+++ disk.sh
@@ -2,5 +2,5 @@
 DISK_USAGE=$(df - hT | grep -vE 'tmp|FILE')
 while IFS= read line
 do 
- usage=$(echo $line | awk '{print$6F}'| cut -d % -f1)
+ usage=$(echo "$line | awk '{print$6F}'| cut -d % -f1")
  done << $DISK_USAGE
