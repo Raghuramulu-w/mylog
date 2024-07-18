064b8f5515481cf682905d7db7fa955538591a93
raghuramulu-w <raghu0711@gmail.com>
2024-07-18 10:33:22 +0530

    hai

--- disk.sh
+++ disk.sh
@@ -5,7 +5,7 @@ while IFS= read line
 do 
  usage=$(echo $line |  awk '{print $6F}' | cut -d % -f1)
  partition=$(echo $line | awk '{print $1F}')
- if [ $partition -ge DISK_TREADSHOLD ]
+ if [ $partition -ge $DISK_TREADSHOLD ]
  then
  echo "high usage $partition : $usage"
  fi
