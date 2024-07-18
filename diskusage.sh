88b2bd61c99fd67aead6f5c014290686bf475bd3
raghuramulu-w <raghu0711@gmail.com>
2024-07-18 11:22:44 +0530

    hai

--- diskusage.sh
+++ diskusage.sh
@@ -3,6 +3,6 @@ DISK_USAGE=$(df -hT | grep -vE'tmp|File')
 DISK_PARTITION=1
 while IFS=read line
 do
-  usage=$(echo $line | awk '{print $6F} | cut -d % -f1)
+  usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
 done<<<$DISK_USAGE
 echo "$usage"
