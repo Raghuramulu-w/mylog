d0152cad0e46db26027e112071961d74cbe1ab0d
raghuramulu-w <raghu0711@gmail.com>
2024-07-18 13:08:52 +0530

    hai

--- logfiledelete.sh
+++ logfiledelete.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-SOURCE_DIR="/tmp/sourceefile.log"
+SOURCE_DIR="/tmp/sourcefile.log"
 if [ ! -d $SOURCE_DIR ]
 then 
    echo "file not exit"
    exit 1
 fi
+while IFS= read -r line
+do 
+log-file=$(echo $line | find. -type f -mtime +14)
+echo " old log files are $log_file
+done<<<$SOURCE_DIR
 
