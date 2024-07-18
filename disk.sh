c69651c81c4d84231c1ef738e8452e8210b4238c
raghuramulu-w <raghu0711@gmail.com>
2024-07-18 11:07:45 +0530

    hai

--- disk.sh
+++ disk.sh
@@ -4,11 +4,11 @@ DISK_TREADSHOLD=1
 message=""
 while IFS= read line
 do 
- usage=$(echo $line |  awk '{print$6F}' | cut -d % -f1)
- partition=$(echo $line | awk '{print$1F}')
+ usage=$(echo $line |  awk '{print $6F}' | cut -d % -f1)
+ partition=$(echo $line | awk '{print $1F}')
  if [ $usage -ge $DISK_TREADSHOLD ]
  then
  message+="high usage $partition on $usage"
  fi
  done <<< $DISK_USAGE
- echo "message :$message"
+ echo -e "message :$message"
