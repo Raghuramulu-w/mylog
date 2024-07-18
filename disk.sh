4f8ac54e5156fd53bbe814d01cfe3b34cb2fdc32
raghuramulu-w <raghu0711@gmail.com>
2024-07-18 09:43:40 +0530

    hai

--- disk.sh
+++ disk.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-DISK_USAGE =$(df -hT | grep -vE 'tmp|File')
-DISK_THRESHOLD=1
-while IFS =read line
+DISK_USAGE=$(df - hT | grep xfs)
+while IFS= read line
 do 
-usage=$(echo $line |awk '{print $6F}'|cut -d % -f1 )
-partition = $(echo $line |  awk '{print $1F}')
-if [ $usage -ge $DISK_THRESHOLD ]
-then  
-    message+="high disk usage on $partitation $usage \n"
-fi
-done
-echo -e " message :$message"
+ usage=$(echo "$line | awk '{print$6F}'| cut -d % -f1")
+ done < $DISK_USAGE
