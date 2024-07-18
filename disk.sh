9eef56bdd348a61f841888a9e04a936f35e742e1
raghuramulu-w <raghu0711@gmail.com>
2024-07-18 09:56:12 +0530

    hai

--- disk.sh
+++ disk.sh
@@ -2,5 +2,5 @@
 DISK_USAGE=$(df - hT | grep xfs)
 while IFS= read line
 do 
- usage=$($line | awk '{print$6F}'| cut -d % -f1)
- done <<< $DISK_USAGE
+ usage=$(echo $line | awk '{print$6F}'| cut -d % -f1)
+ done < $DISK_USAGE
