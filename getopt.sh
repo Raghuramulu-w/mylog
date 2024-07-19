1f97ef11334a58329e23113df47dd2900e631eba
raghuramulu-w <raghu0711@gmail.com>
2024-07-19 15:18:30 +0530

    hai

---
+++ getopt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+FRUITNAMES=""
+VEGITABLES=""
+while getopt ":f:veg:h" org 
+do
+    case $org
+      f) FRUITNAMES="$OPTARGS";;
+      veg) VEGITABLES="$OPTARGS";;
+       h) USAGE ; exit 1
+       \?) echo "invalid option USAGE exit"
+    esac
+done
+if [ -z $FRUITNAMES ] || [ -z $VEGITABLES ]
+then
+echo "both are manditory to enter"
+USAGE
+exit
+fi
+echo "fruits names are $FRUITNAMES vegitable name are $VEGITABLES "
