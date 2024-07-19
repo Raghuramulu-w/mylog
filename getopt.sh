53eb9ba41cf19d71df5dc610ee4ef5c146dac229
raghuramulu-w <raghu0711@gmail.com>
2024-07-19 16:07:45 +0530

    hi

--- getopt.sh
+++ getopt.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 FRUITNAMES=""
 VEGITABLES=""
+USAGE(){
+echo "usage :: $(basename $0) -n 
+echo"option"
+echo "FRUITNAMES specify fruits"
+echo "vegitables specify vegies"
+
+}
 while getopt ":f:veg:h" org 
 do
     case $org in
       f) FRUITNAMES="$OPTARGS";;
       veg) VEGITABLES="$OPTARGS";;
        h) USAGE ; exit 1
-       \?) echo "invalid option USAGE exit"
+       #\?) echo "invalid option USAGE exit"
     esac
 done
 if [ -z $FRUITNAMES ] || [ -z $VEGITABLES ]
