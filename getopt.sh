6f7435bc0027f77e63b737063d081a7c8367b9ff
raghuramulu-w <raghu0711@gmail.com>
2024-07-19 16:15:17 +0530

    hi

--- getopt.sh
+++ getopt.sh
@@ -8,8 +8,7 @@ echo "FRUITNAMES specify fruits"
 echo "vegitables specify vegies"
 
 }
-while getopt ":f:veg:h" org ;
-do
+while getopt ":f:veg:h" org ;do
     case $org in
       f) FRUITNAMES="$OPTARGS";;
       veg) VEGITABLES="$OPTARGS";;
