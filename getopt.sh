39281e5ce15a6c554f854bba4eda37d60b114464
raghuramulu-w <raghu0711@gmail.com>
2024-07-19 16:50:54 +0530

    hi

--- getopt.sh
+++ getopt.sh
@@ -2,7 +2,7 @@
 FRUITNAMES=""
 VEGITABLES=""
 USAGE(){
-echo "usage :: $(basename $0) " 
+echo "usage :: $(basename $0) -f FRUITNAMES -v VEGITABLES" 
 echo "option"
 echo "FRUITNAMES specify fruits"
 echo "vegitables specify vegies"
